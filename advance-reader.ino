b1f4248058005612b8ac0cd2317833d8da14b724
sz2 <sugiura.k.nigo@gmail.com>
2022-04-25 00:42:01 +0900

    Create habdleMeterTypeQuery.

--- advance-reader.ino
+++ advance-reader.ino
@@ -11,7 +11,7 @@ const byte INIT_STEP1 = 0x11;
 const byte INIT_STEP2 = 0x21;
 const byte INIT_STEP3 = 0x31;
 
-
+#define SIZEM(a)  (sizeof((a)) / sizeof((a[0])))
 CustomSoftwareSerial* customSerial;  
 
 void setup() {
@@ -37,6 +37,20 @@ void loop() {
 
 }
 
+void skipRead(CustomSoftwareSerial* customSerial, int bytes)
+{
+  for(int i = 0; i < bytes; i++)
+    customSerial->read();
+}
+
+void sendData(byte dat[], int siz) 
+{
+  for(int i = 0; i < siz; i++) 
+  {
+    customSerial->write(dat[i]);
+  }
+}
+
 void findDelimiter(CustomSoftwareSerial* customSerial)
 {
   if(DEBUG)
@@ -53,7 +67,7 @@ void findDelimiter(CustomSoftwareSerial* customSerial)
     Serial.println("Delimiter is found.");
 }
 
-void handleInitializeFrame(CustomSoftwareSerial customSerial)
+void handleInitializeFrame(CustomSoftwareSerial* customSerial)
 {
   if(DEBUG)
     Serial.println("Processing INITIALIZE message(0x01)");
@@ -76,6 +90,59 @@ void handleInitializeFrame(CustomSoftwareSerial customSerial)
     case INIT_STEP3:
       if(DEBUG)
         Serial.println("INITIALIZE_STEP3.");
+      break;
+  }
+}
+
+void handleMeterTypeQuery(CustomSoftwareSerial* customSerial)
+{
+  if(DEBUG)
+    Serial.println("Handle meter type query.");
+
+  skipRead(customSerial, 20);
+  byte meterID = customSerial->read();
+  byte[] returnData = {0, 0xFF, 0xFF, 0x0A, 0x00, 0, 0};
+
+  switch(meterID)
+  {
+    case 0x01:
+      if(DEBUG)
+        Serial.println("Return meter type ID 1 = 0x01(Boost).");
+
+      retunData[4] = 0x01; // Boost
+      sendData(retunData, SIZEM(retunData));
+      break;
+    case 0x02:
+      if(DEBUG)
+        Serial.println("Return meter type ID 2 = 0x06(Rev).");
+
+      retunData[4] = 0x06; // Rev
+      sendData(retunData, SIZEM(retunData));
+      break;
+    case 0x03:
+      if(DEBUG)
+        Serial.println("Return meter type ID 3 = 0x09(Oil pres).");
+
+      retunData[4] = 0x09; // Oil pres
+      sendData(retunData, SIZEM(retunData));
+      break;
+    case 0x04:
+      if(DEBUG)
+        Serial.println("Return meter type ID 4 = 0x0B(Oil temp).");
+
+      retunData[4] = 0x0B; // Oil temp
+      sendData(retunData, SIZEM(retunData));
+      break;
+    case 0x05:
+      if(DEBUG)
+        Serial.println("Return meter type ID 5 = 0x0c(Water temp).");
+
+      retunData[4] = 0x0C; // Water temp
+      sendData(retunData, SIZEM(retunData));
+      break;
+    default;
+       if(DEBUG)
+        Serial.println("Meter query type is not implemented.");
       break;
   }
 }
